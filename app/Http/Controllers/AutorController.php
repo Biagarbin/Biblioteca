0884f7889f6afc2961e9cccb58d4a18e16d60e21
BiaGarbin <biasrgarbin@gmail.com>
2025-09-19 15:50:55 -0300

    adicionado delete show update

--- app/Http/Controllers/AutorController.php
+++ app/Http/Controllers/AutorController.php
@@ -4,22 +4,71 @@ namespace App\Http\Controllers;
 
 use App\Models\Autor;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 
 class AutorController extends Controller
 {
- public function store(Request $request){
-     $autor = Autor::create([
-    'nome_completo'=>$request->nome_completo,
-    'data_nascimento'=>$request->data_nascimento,
-    'nacionalidade'=>$request->nacionalidade,
+    public function store(Request $request)
+    {
+        $autor = Autor::create([
+            'nome_completo' => $request->nome_completo,
+            'data_nascimento' => $request->data_nascimento,
+            'nacionalidade' => $request->nacionalidade,
 
         ]);
-        return response()->json($autor); 
-}
-    
-public function index(){
-    $autor = Autor::all();
+        return response()->json($autor);
+    }
 
-    return response()->json($autor);
+    public function index()
+    {
+        $autor = Autor::all();
+
+        return response()->json($autor);
+    }
+    public function update (Request $request)
+
+    $autor = Autor::find($request->id);
+    if ($autor == null) {
+    return response()->json([
+        'erro' => 'Tarefa não encontrada'
+    ]);
  }
+
+if(isset($request->nome_completo)){
+    $autor->nome_completo = $request->nome_completo;
+}
+if (isset($request->data_nascimento)){
+    $autor->data_nascimento = $request->data_nascimento;
+}
+if(isset($request->nacionalidade)){
+    $autor->nacionaliade = $request->nacionalidade;
+}
+$autor->update();
+return response ()->json([
+    'mensagem' => 'atualizada'
+]);
+}
+public function show($id)
+{
+    $autor = Autor::find($id);
+    if($autor == null){
+        return response()->json([
+            'erro' => 'Autor não encontrado'
+        ]);
+    }
+    return response()->json($autor);
+}
+public function delete($id)
+{
+
+$autor = Autor::find($id);
+if($autor == null){
+    return response ()->json([
+        'erro' => 'Autor não encontrada'
+    ]);
+}
+$autor->delete();
+return response ()->json([
+    'mensagem' => 'excluído'
+]);
 }
