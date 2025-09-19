6fcdac6b59e41714b2c2d5bb724b9e493fcfe481
BiaGarbin <biasrgarbin@gmail.com>
2025-09-19 15:51:22 -0300

    adicionado show update delete

--- app/Http/Controllers/AutorController.php
+++ app/Http/Controllers/AutorController.php
@@ -25,50 +25,52 @@ class AutorController extends Controller
 
         return response()->json($autor);
     }
-    public function update (Request $request)
+    public function update(Request $request)
+    {
 
-    $autor = Autor::find($request->id);
-    if ($autor == null) {
-    return response()->json([
-        'erro' => 'Tarefa não encontrada'
-    ]);
- }
+        $autor = Autor::find($request->id);
+        if ($autor == null) {
+            return response()->json([
+                'erro' => 'Tarefa não encontrada'
+            ]);
+        }
 
-if(isset($request->nome_completo)){
-    $autor->nome_completo = $request->nome_completo;
-}
-if (isset($request->data_nascimento)){
-    $autor->data_nascimento = $request->data_nascimento;
-}
-if(isset($request->nacionalidade)){
-    $autor->nacionaliade = $request->nacionalidade;
-}
-$autor->update();
-return response ()->json([
-    'mensagem' => 'atualizada'
-]);
-}
-public function show($id)
-{
-    $autor = Autor::find($id);
-    if($autor == null){
+        if (isset($request->nome_completo)) {
+            $autor->nome_completo = $request->nome_completo;
+        }
+        if (isset($request->data_nascimento)) {
+            $autor->data_nascimento = $request->data_nascimento;
+        }
+        if (isset($request->nacionalidade)) {
+            $autor->nacionaliade = $request->nacionalidade;
+        }
+        $autor->update();
         return response()->json([
-            'erro' => 'Autor não encontrado'
+            'mensagem' => 'atualizada'
         ]);
     }
-    return response()->json($autor);
-}
-public function delete($id)
-{
+    public function show($id)
+    {
+        $autor = Autor::find($id);
+        if ($autor == null) {
+            return response()->json([
+                'erro' => 'Autor não encontrado'
+            ]);
+        }
+        return response()->json($autor);
+    }
+    public function delete($id)
+    {
 
-$autor = Autor::find($id);
-if($autor == null){
-    return response ()->json([
-        'erro' => 'Autor não encontrada'
-    ]);
-}
-$autor->delete();
-return response ()->json([
-    'mensagem' => 'excluído'
-]);
+        $autor = Autor::find($id);
+        if ($autor == null) {
+            return response()->json([
+                'erro' => 'Autor não encontrada'
+            ]);
+        }
+        $autor->delete();
+        return response()->json([
+            'mensagem' => 'excluído'
+        ]);
+    }
 }
--- app/Http/Controllers/EmprestimoController.php
+++ app/Http/Controllers/EmprestimoController.php
@@ -4,22 +4,71 @@ namespace App\Http\Controllers;
 
 use App\Models\Emprestimo;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 
 class EmprestimoController extends Controller
 {
-    public function store (Request $request){
-     $emprestimo = Emprestimo::create([
+public function store (Request $request){
+    $emprestimo = Emprestimo::create([
     'data_emprestimo'=>$request->data_emprestimo,
     'data_devolucao'=>$request->data_emprestimo,
     'codigo_membro'=>$request->codigo_membro,
     'codigo_livro'=>$request->codigo_livro,
 
     ]);
-        return response()->json($emprestimo); 
+    return response()->json($emprestimo); 
     }
-    public function index(){
+public function index(){
     $emprestimo= Emprestimo::all();
 
     return response()->json($emprestimo);
  }
+
+public function update(Request $request){
+
+    $emprestimo= Emprestimo::find($request->id);
+    if ($emprestimo == null) {
+    return response()->json([
+        'erro' => 'Emprestimo não encontrado'
+        ]);
+    }
+
+    if (isset($request->data_emprestimo)) {
+    $emprestimo->data_emprestimo= $request->data_emprestimo;
+    }
+    if (isset($request->data_devolucao)) {
+    $emprestimo->data_devolucao = $request->data_devolucao;
+     }
+    if (isset($request->codigo_membro)) {
+    $emprestimo->codigo_membro= $request->codigo_membro;
+    }
+    if(isset($request->codigo_livro)){
+    $emprestimo->codigo_livro=$request->codigo_livro;
+    }
+    $emprestimo->update();
+    return response()->json([
+    'mensagem' => 'atualizada'
+    ]);
+    }
+public function show($id){
+    $emprestimo = Emprestimo::find($id);
+    if ($emprestimo == null) {
+    return response()->json([
+    'erro' => 'Emprestimo não encontrado'
+    ]);
+    }
+        return response()->json($emprestimo);
+    }
+public function delete($id){
+    $emprestimo = Emprestimo::find($id);
+    if ($emprestimo == null) {
+    return response()->json([
+    'erro' => 'Emprestimo não encontrado'
+    ]);
+    }
+    $emprestimo->delete();
+    return response()->json([
+    'mensagem' => 'excluído'
+    ]);
+    }
 }
